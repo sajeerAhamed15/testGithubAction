e58d059f1723e53f66ef2f55f10888b3d91b0d40
sajeerAhamed15 <ahamedsajeer.15@cse.mrt.ac.lk>
2020-02-26 10:54:36 +0530

    test

--- pythonScript.py
+++ pythonScript.py
@@ -1,2 +1,12 @@
 import sys
+import os
+
 print ("hello " + sys.argv[1])
+
+baseDir = os.path.dirname(os.path.realpath(__file__))
+subDir = "/target/output/"
+outputJson_rel = "release_notes_versions.txt"
+outputFile_rel = baseDir + subDir + outputJson_rel
+os.makedirs(os.path.dirname(outputFile_rel), exist_ok=True)
+with open(outputFile_rel, "w") as f:
+    f.write("hihihih")
